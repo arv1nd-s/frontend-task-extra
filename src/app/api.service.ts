ae2f2e6ba9b0d3472959f8e2df2e9665ba575208
Arvind Suthar <arvindsuthar198@gmail.com>
2023-06-22 23:42:00 +0530

    api calls | constructors

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getPanelData(){
+    return this.http.get('https://uat.utopiatech.in:4520/panel/gettestlist?org_id=3');
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend-task-extra';
+
+  panelData = null;
+  constructor(private api:ApiService) {}
+
+  ngOnInit() {
+    this.api.getPanelData().subscribe((data: any)=>{
+      this.panelData = data['result'];
+    })
+  }
 }
